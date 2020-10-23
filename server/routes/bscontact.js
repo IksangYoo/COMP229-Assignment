2444af1e065c449c17774ab3859758dde0728621
IksangYoo <andrew4477@naver.com>
2020-10-24 04:45:21 +0900

    login page

--- server/routes/bscontact.js
+++ server/routes/bscontact.js
@@ -2,29 +2,37 @@ let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
 
-// Connect to our bscontact Model
-let Bscontact = require('../models/bscontact');
+let passport = require('passport');
 
 let bscontactController = require('../controllers/bscontact');
+
+// helper function for guard purposes
+function requireAuth(req, res, next)
+{
+    //check if the user is logged in
+    if(!req.isAuthenticated())
+    {
+        return res.redirect('/login');    
+    }
+    next();
+}
 
 /* Get Route for the Business contact list page - READ Operation */
 router.get('/', bscontactController.displayBsContactList);
 
-
 /* GET Route for displaying Add page - CREATE Opreation */
-router.get('/add', bscontactController.displayAddPage);
+router.get('/add', requireAuth, bscontactController.displayAddPage);
 
 /* POST Route for processing Add page - CREATE Opreation */
-router.post('/add', bscontactController.processAddPage);
+router.post('/add', requireAuth, bscontactController.processAddPage);
 
 /* GET Route for displaying Edit page - UPDATE Opreation */
-router.get('/edit/:id', bscontactController.displayEditPage);
+router.get('/edit/:id', requireAuth, bscontactController.displayEditPage);
 
 /* POST Route for processing Edit page - UPDATE Opreation */
-router.post('/edit/:id', bscontactController.processEditPage);
-
+router.post('/edit/:id', requireAuth, bscontactController.processEditPage);
 
 /* GET to perform Deletion - DELETE Opreation */
-router.get('/delete/:id', bscontactController.performDeletePage);
+router.get('/delete/:id', requireAuth, bscontactController.performDeletePage);
 
 module.exports = router;
